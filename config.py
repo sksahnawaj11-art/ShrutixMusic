09b1c23119fec7b46569255c77520fb2251e73bf
Simple music repo <sksahnawaj11@gmail.com>
2026-05-08 18:49:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,8 +83,8 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/c8b09659c837cd9142601-f594bfd29076e79944.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+PLAYLIST_IMG_URL = "https://img.sanishtech.com/u/b864da44368bacccd4c2a8176ec816c8.jpg"
+STATS_IMG_URL = "https://img.sanishtech.com/u/b864da44368bacccd4c2a8176ec816c8.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
